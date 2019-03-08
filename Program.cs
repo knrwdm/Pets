a0036e50913e33af14f559eefb49ce2c5ac66197
Kayla Reap <42559069+knrwdm@users.noreply.github.com>
2019-03-08 15:22:20 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Pets pet1 = new Pets("dog", "Shadow", "Jose", 42.6);
+
+            Console.WriteLine("Name: " + pet1.name);
+            Console.WriteLine("Weight: " + pet1.weight);
+            Console.WriteLine(pet1.getTag());
+
+            Console.WriteLine("");
+
+            Dog dog1 = new Dog("Daisy", "Kent", 23.4);
+
+            Console.WriteLine("Name: " + dog1.name);
+            Console.WriteLine("Weight: " + dog1.weight);
+            Console.WriteLine(dog1.getTag());
+            Console.WriteLine(dog1.bark(4));
+
+            Console.WriteLine("");
+
+            Cat cat1 = new Cat("Simba", "Maria", 5.2);
+
+            Console.WriteLine("Name: " + cat1.name);
+            Console.WriteLine("Weight: " + cat1.weight);
+            Console.WriteLine(cat1.getTag());
+            Console.WriteLine(cat1.meow(3));
+        }
+    }
+}
